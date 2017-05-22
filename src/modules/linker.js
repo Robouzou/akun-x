6950f88661ff92db651bec357e0583587c239a75
Fiddlekins <fiddlekins@gmail.com>
2017-05-22 09:48:37 +0100

    Fixed bug where image posts would cause errors

--- src/modules/linker.js
+++ src/modules/linker.js
@@ -18,17 +18,19 @@ export default class Linker {
 		if (!node.dataset[Linker.id]) {
 			node.dataset[Linker.id] = true;
 			let contentNode = node.querySelector('.fieldBody');
-			for (let node of contentNode.childNodes) {
-				if (node.nodeType === Node.TEXT_NODE) {
-					// Only touch text nodes to avoid interfering with any HTML
-					let urlMatch = /https?:\/\/[A-z0-9\-\._~:\/\?#\[\]@\!\$&'\(\)*\+,;=%]+/.exec(node.nodeValue);
-					if (urlMatch) {
-						let newLink = document.createElement('a');
-						let range = new Range();
-						range.setStart(node, urlMatch.index);
-						range.setEnd(node, urlMatch.index + urlMatch[0].length);
-						range.surroundContents(newLink);
-						newLink.href = urlMatch[0];
+			if (contentNode) {
+				for (let node of contentNode.childNodes) {
+					if (node.nodeType === Node.TEXT_NODE) {
+						// Only touch text nodes to avoid interfering with any HTML
+						let urlMatch = /https?:\/\/[A-z0-9\-\._~:\/\?#\[\]@\!\$&'\(\)*\+,;=%]+/.exec(node.nodeValue);
+						if (urlMatch) {
+							let newLink = document.createElement('a');
+							let range = new Range();
+							range.setStart(node, urlMatch.index);
+							range.setEnd(node, urlMatch.index + urlMatch[0].length);
+							range.surroundContents(newLink);
+							newLink.href = urlMatch[0];
+						}
 					}
 				}
 			}
