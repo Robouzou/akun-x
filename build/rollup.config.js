572329ab76f57e92ca746d6c16ef8a24930f1949
Fiddlekins <fiddlekins@gmail.com>
2017-05-22 09:43:12 +0100

    Added node module resolution

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,13 +1,22 @@
 'use strict';
 
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 
 module.exports = {
 	entry: 'src/index.js',
 	format: 'iife',
 	plugins: [
+		nodeResolve({
+			jsnext: true,
+			browser: true,
+			extensions: ['.js', '.json']
+		}),
+		commonjs({
+			sourceMap: false
+		}),
 		babel({
-			// exclude: 'node_modules/**' // only transpile our source code
 		})
 	],
 	dest: 'bundle.js'
